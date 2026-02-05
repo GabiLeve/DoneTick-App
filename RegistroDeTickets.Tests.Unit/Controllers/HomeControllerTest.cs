0ed8f26ecd3f4f66b0336a8f333ea38c204dd230
Gabriela <leverattomariag@gmail.com>
2026-02-05 19:33:42 -0300

    test: agrego carpetas para tests y test de HomeController

---
+++ RegistroDeTickets.Tests.Unit/Controllers/HomeControllerTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RegistroDeTickets.web.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegistroDeTickets.Tests.Unit.Controllers
+{
+    public class HomeControllerTests
+    {
+        [Fact]
+        public void Index_DebeRetornarLaVistaPredeterminada()
+        {
+            var controller = new HomeController();
+
+            var result = controller.Index();
+
+            var viewResult = Assert.IsType<ViewResult>(result);
+            Assert.Null(viewResult.ViewName);
+        }
+    }
+}
